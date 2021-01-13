f1ddaba1a6718a3346017ffbe519967e44557995
GalstyanDavi <63391575+GalstyanDavi@users.noreply.github.com>
2021-01-14 02:14:19 +0400

    First commit
    Added Unity game to GitHub

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void startscene()
+    {
+        Application.LoadLevel("MainScene");
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+    public void menuscene()
+    {
+        Application.LoadLevel("MainMenu");
+    }
+}
